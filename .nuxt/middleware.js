d0dc5af8b43f406a246d845075f021ecc0598685
Xuanhee <529177573@qq.com>
2020-10-15 11:11:12 +0800

    打包

--- .nuxt/middleware.js
+++
@@ -1,9 +0,0 @@
-const middleware = {}
-
-middleware['authenticated'] = require('..\\middleware\\authenticated.js')
-middleware['authenticated'] = middleware['authenticated'].default || middleware['authenticated']
-
-middleware['notauthenticated'] = require('..\\middleware\\notauthenticated.js')
-middleware['notauthenticated'] = middleware['notauthenticated'].default || middleware['notauthenticated']
-
-export default middleware
